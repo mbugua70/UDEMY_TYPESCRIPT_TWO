e9cad65beba8f3199d9f367b4a155a22612f49ac
john mbugua <johnymbugua1997@gmail.com>
2024-06-29 23:01:28 +0300

    Modern javascript and Typescript Array destructuring and Object destructuring

--- modernjs/dist/modern.js
+++ modernjs/dist/modern.js
@@ -1,1 +1,18 @@
 "use strict";
+if (4 > 1) {
+    let a = 2;
+}
+const add = (a, b) => a + b;
+const printOutput = (output) => console.log(output);
+printOutput(add(2, 4));
+const addTwo = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const resultTwo = addTwo(2, 4, 4, 4, 4, 5.5);
+console.log(resultTwo);
+const hobbies = ["john", "mary", "ceaser", "david"];
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1);
+console.log(hobby2);
--- modernjs/src/modern.ts
+++ modernjs/src/modern.ts
@@ -8,3 +8,36 @@ if (4 > 1) {
 }
 
 // console.log(a);
+
+
+const add = (a: number, b: number) => a + b;
+
+// const printOutput = (output: string | number) => console.log(output);
+
+// The above arrow function can be written as shown below
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+printOutput(add(2, 4));
+
+// Rest parameter
+
+const addTwo = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const resultTwo = addTwo(2, 4, 4, 4, 4, 5.5);
+console.log(resultTwo);
+
+// Destructuring of Arrays and Objects
+// pulling items out of an array or objects
+// in array element are pulled out in order
+// while in object element are not pulled out in order but according to the key.
+
+const hobbies = ["john", "mary", "ceaser", "david"];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1);
+console.log(hobby2);
